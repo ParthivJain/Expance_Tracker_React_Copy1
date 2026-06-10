eccc7a75d7988aa22b4c553cf1aa4db23d7606c1
ParthivJain <jainparthiv2@gmail.com>
2026-06-10 14:44:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/Expance_Tracker_React_Copy1/'
 })
